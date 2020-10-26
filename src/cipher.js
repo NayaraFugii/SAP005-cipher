c883ae3a96e300beea614328f86b2fbe42d5f66c
CaioFugii <caio.fugii@hotmail.com>
2020-10-26 17:56:18 -0300

    inclusao-de-const-e-front

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,6 @@
-const cipher = {
-  // ...
-};
+const cipher = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+const formula = (charCodeAt()+ 5) % 25;
 
-export default cipher;
+for(let i = 0; i < cipher.length; i++){
+console.log(formula);
+}
