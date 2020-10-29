e9696cd5c4dd951ea78e3e18bfefe51ea078eb15
Nayara.Fugii <nayarac.neto@gmail.com>
2020-10-28 22:25:07 -0300

    inclusao-da-funçao

--- src/cipher.js
+++ src/cipher.js
@@ -1,24 +1,34 @@
 
 const cipher = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-let string = 'ABC DEF GHI';
+let string = 'LILLA L';
 let offset = parseInt('1');
 let text = '';
 let space = ' ';
 
+function cifra(){
 
-   
-for(let f = 0; f <= string.length; f++){
-    if(string[f] == space){
-        text = text + space
+    if(offset == 0){
+        alert('Não é possivel cifrar a mensagem, pois o número de casas é igual a zero.');
+        return;
     }
 
-    for(let i = 0; i < cipher.length; i++){   
-
-        if(cipher[i] == string[f]){
-            let code = (string.charCodeAt(f) - 65 + offset) % 26
-            text = text + cipher[code];
+    for(let f = 0; f <= string.length; f++){
+        if(string[f] == space){
+            text = text + space
         }
-                   
+    
+        for(let i = 0; i < cipher.length; i++){   
+    
+            if(cipher[i] == string[f]){
+                let code = (string.charCodeAt(f) - 65 + offset) % 26
+                text = text + cipher[code];
+            }
+                       
+        }
     }
+    
 }
-console.log(text);
+    cifra();
+
+    console.log(text);
+ 
