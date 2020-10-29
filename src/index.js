cb1eeec7f083511d525b73f7fe683c6f06b4ba10
Nayara.Fugii <nayarac.neto@gmail.com>
2020-10-29 20:21:46 -0300

    dom-interacao-das-paginas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
-function (){
+const code = document.getElementById('buttonCode');
+const answer = document.getElementById('text')
 
-}
+code.addEventListener('click', () => { 
+    let area = document.getElementById('text').value 
+    let off = document.getElementById('offset').value
+    answer.textContent = cipher(area, off)
+    console.log(area);
+});
 
