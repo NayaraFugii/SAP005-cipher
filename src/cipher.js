0dcf946d77bd00bab581c3e5511e6a44697ed354
Nayara.Fugii <nayarac.neto@gmail.com>
2020-10-30 17:29:13 -0300

    inclusao-do-dom-na-primeira-tela

--- src/cipher.js
+++ src/cipher.js
@@ -1,33 +1,36 @@
-
-const cipher = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-let string = 'NAY ARA';
-let offset = parseInt('2');
-let text = '';
+const alfabeto = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 const space = ' ';
 
-function cifra(){  
+const cipher = {
+     cifra(offset,message){  
+       // let string = message;
+        //let offset = parseInt('2');
+        let text ='';
+        
+        if(offset === 0){
+            alert('Não é possivel cifrar a mensagem, pois o número de casas é igual a zero.');
+            return;
+        }
     
-    if(offset === 0){
-        alert('Não é possivel cifrar a mensagem, pois o número de casas é igual a zero.');
-        return;
-    }
+        for(let f = 0; f <=message.length; f++){
+            if(message[f] == space){
+                text = text + space
+            }
+        
+            for(let i = 0; i < alfabeto.length; i++){   
+                if(alfabeto[i] == message[f]){
+                    let formula = (message.charCodeAt(f) - 65 + offset) % 26
+                    text = text + alfabeto[formula];
+                }         
+            }
+        }
+        return text;
+    },
+}
 
-    for(let f = 0; f <= string.length; f++){
-        if(string[f] == space){
-            text = text + space
-        }
-    
-        for(let i = 0; i < cipher.length; i++){   
-            if(cipher[i] == string[f]){
-                let formula = (string.charCodeAt(f) - 65 + offset) % 26
-                text = text + cipher[formula];
-            }         
-        }
-    } console.log(text);
-    //return text;
-};
+
  
  
- cifra();
+ //cifra();
  
-export default cifra;
+export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
 import cipher from './cipher.js';
 
 const code = document.getElementById('buttonCode')
-const answer = document.getElementById('text')
+const answer = document.getElementById('answer')
+const psw = document.getElementById('password')
+const decode = document.getElementById('buttonDecode')
+const frase = document.getElementById('text')
 
-code.addEventListener('click', () => { 
-    let area = document.getElementById('text').value 
+
+code.addEventListener('click', (event) => { 
+  event.preventDefault()
+    let area = document.getElementById('answer').value 
     let off = document.getElementById('offset').value
-    answer.textContent = cipher(area)
-    console.log(a);
+    let password = document.getElementById('password').value
+    console.log(password);
+    answer.value = cipher.cifra(off,area)  
+
+    
 });
 
-//const decode = document.getElementById('buttonDecode')
-//const password = document.getElementById('password').value
 
+decode.addEventListener('click', (event)=>{
+  event.preventDefault()
+  let area = document.getElementById('answer').value 
+  let password = document.getElementById('password').value
+  const psw = document.getElementById('password').value
+  console.log(psw)
+    if (password == psw ){
+      frase.textContent = area.value;
+      
+    }else{
+      alert('A senha inserida está incorreta');
+     }   
 
-//decode.addEventListener('click', function(){
-
-  //  if (password === true){
-        //answer.textContent = area
-
- //   }
-
-//});
+});
