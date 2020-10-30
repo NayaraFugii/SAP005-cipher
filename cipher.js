59a9dce88e52ea06b78ff2a28167dd10570950e9
Nayara.Fugii <nayarac.neto@gmail.com>
2020-10-30 14:35:28 -0300

    logica-pronta-inclusao-do-dom

--- cipher.js
+++ cipher.js
@@ -1,15 +1,13 @@
 
 const cipher = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-
+let string = 'NAY ARA';
+let offset = parseInt('2');
+let text = '';
 const space = ' ';
 
-
-function cifra(area, off){
-    let string = area
-    let offset = parseInt(off)
-    let text = ''
-
-    if(offset == 0){
+function cifra(){  
+    
+    if(offset === 0){
         alert('Não é possivel cifrar a mensagem, pois o número de casas é igual a zero.');
         return;
     }
@@ -20,17 +18,16 @@ function cifra(area, off){
         }
     
         for(let i = 0; i < cipher.length; i++){   
-    
             if(cipher[i] == string[f]){
                 let formula = (string.charCodeAt(f) - 65 + offset) % 26
                 text = text + cipher[formula];
-                
-            }
-                       
+            }         
         }
-    } 
-    return text;
-}
-
+    } console.log(text);
+    //return text;
+};
+ 
+ 
+ cifra();
  
 export default cifra;
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import cipher from './cipher.js';
 
-const code = document.getElementById('buttonCode');
+const code = document.getElementById('buttonCode')
 const answer = document.getElementById('text')
 
 code.addEventListener('click', () => { 
     let area = document.getElementById('text').value 
     let off = document.getElementById('offset').value
-    answer.textContent = cipher(area, off)
-    console.log(area);
+    answer.textContent = cipher(area)
+    console.log(a);
 });
 
+//const decode = document.getElementById('buttonDecode')
+//const password = document.getElementById('password').value
+
+
+//decode.addEventListener('click', function(){
+
+  //  if (password === true){
+        //answer.textContent = area
+
+ //   }
+
+//});
