491501874abf7e3111581af14442ed2116013d96
Nayara.Fugii <nayarac.neto@gmail.com>
2020-11-05 18:34:05 -0300

    ajuste-da-inteface

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import cipher from './cipher.js';
 
 const code = document.getElementById('buttonCode')
 const answer = document.getElementById('answer')
-const psw = document.getElementById('password')
 const decode = document.getElementById('buttonDecode')
-const frase = document.getElementById('text')
+
 
 
 code.addEventListener('click', (event) => { 
   event.preventDefault()
     let area = document.getElementById('answer').value     
-    let off = document.getElementById('offset').value    
-    let password = document.getElementById('password').value    
+    let off = document.getElementById('offset').value 
     answer.value = cipher.cifra(off,area) 
 
     
